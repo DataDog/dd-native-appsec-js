553e373811fa5ef3abd8d33a40abc7a700bfb2d3
simon-id <simon.id@datadoghq.com>
2022-04-17 21:41:35 +0200

    test linter

--- src/convert.h
+++ src/convert.h
@@ -9,6 +9,6 @@
 #include <ddwaf.h>
 
 ddwaf_object* to_ddwaf_object(ddwaf_object *object,
-                        Napi::Env env, Napi::Value val, int depth, bool lim);
+                            Napi::Env env, Napi::Value val, int depth, bool lim);
 
 #endif  // SRC_CONVERT_H_
